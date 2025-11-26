c7b51129405876e2bc352d0cb65c1267cfc442ac
mbekkhus-creator <m.bekkhus@gmail.com>
2025-11-26 14:23:26 +0100

    added JavaScript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+const hamburger = document.querySelector('.hamburger');
+  const menu = document.getElementById('mobileMenu');
+  const closeBtn = document.querySelector('.menu-close');
+
+  function openMenu() {
+    menu.classList.add('open');
+    document.body.classList.add('menu-open');
+    menu.setAttribute('aria-hidden', 'false');
+  }
+
+  function closeMenu() {
+    menu.classList.remove('open');
+    document.body.classList.remove('menu-open');
+    menu.setAttribute('aria-hidden', 'true');
+  }
+
+  hamburger.addEventListener('click', openMenu);
+  closeBtn.addEventListener('click', closeMenu);
+
+  // Lukke på ESC
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') closeMenu();
+  });
+
+
 // Finn handlekurv-teller
 const cartCountEl = document.getElementById('cart-count');
 let cartCount = 0;
